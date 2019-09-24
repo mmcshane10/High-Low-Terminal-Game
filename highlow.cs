ca4b180f56396585a515bd09d881fc0e1c5ef4cb
Erin Deuel, joel stockamp and Mike McShane <erin@>
2019-09-24 11:47:40 -0700

    add ascii art and endgame looping

--- highlow.cs
+++ highlow.cs
@@ -22,17 +22,26 @@ class Game
 
 		public void MakeComputerGuess()
 		{
-			if (Turns < 5)
+			if (Turns < 6)
 			{
-				ComputerGuess = (MinValue + MaxValue) / 2;
-				Console.WriteLine("My guess is: " + ComputerGuess);
+				Random rand = new Random ();
+				int offset = rand.Next(-4, 4);
+				int intialGuess = (MinValue + MaxValue) / 2;
+				ComputerGuess = intialGuess + offset;
+				if (ComputerGuess > MaxValue || ComputerGuess < MinValue)
+				{
+					ComputerGuess = intialGuess;
+				}
 				Turns++;
-				Console.WriteLine("This is guesss number " + Turns);			
+				Console.WriteLine("This is guess number " + Turns);	
+				Console.WriteLine("My guess is: " + ComputerGuess);
 				UpdateRange();
 			}
 			else
 			{
-				Console.WriteLine("Even the blind squirrel finds a nut occasionally. You win, " + Name + "!");
+				Console.WriteLine("You have defeated the mighty computer. You win, " + Name + "!");
+				Console.WriteLine("Lets play again");
+				StartGame();
 			}
 		}
 
@@ -43,11 +52,29 @@ class Game
 
 			if (userFeedback == "H" || userFeedback == "h")
 			{
-				MinValue = ComputerGuess;
+				if (Number > ComputerGuess)
+				{
+					MinValue = ComputerGuess;
+				}
+				else
+				{
+					Console.WriteLine("Why must you always lie.");
+					EndGame();
+					return;
+				}
 			}
 			else if (userFeedback == "L" || userFeedback == "l")
 			{
+				if (Number < ComputerGuess)
+				{
 				MaxValue = ComputerGuess;
+				}
+				else
+				{
+					Console.WriteLine("Why must you always lie.");
+					EndGame();
+					return;
+				}
 			}
 			else if (userFeedback == "C" || userFeedback == "c")
 			{
@@ -60,6 +87,7 @@ class Game
 				{
 					Console.WriteLine("That wasn't your number, moron!");
 					EndGame();
+					return;
 				}
 			} 
 			else
@@ -71,18 +99,41 @@ class Game
 		}
 		public void EndGame()
 		{
-			Console.WriteLine("You Lost, you scruffy-looking nerf-herder.");
-			// StartGame();
+			Console.WriteLine("You lost " + Name + ", you scruffy-looking nerf-herder.");
+			var arr = new[]
+            {
+						@"888888888888888888888888888888888888888888888888888888888888",
+						@"888888888888888888888888888888888888888888888888888888888888",
+						@"8888888888888888888888888P''  ''9888888888888888888888888888",
+						@"8888888888888888P'88888P          988888'9888888888888888888",
+						@"8888888888888888  '9888            888P'  888888888888888888",
+						@"888888888888888888bo '9  d8o  o8b  P' od88888888888888888888",
+						@"888888888888888888888bob 98'  '8P dod88888888888888888888888",
+						@"888888888888888888888888    db    88888888888888888888888888",
+						@"88888888888888888888888888      8888888888888888888888888888",
+						@"88888888888888888888888P'9bo  odP'98888888888888888888888888",
+						@"88888888888888888888P' od88888888bo '98888888888888888888888",
+						@"888888888888888888   d88888888888888b   88888888888888888888",
+						@"8888888888888888888oo8888888888888888oo888888888888888888888",
+						@"888888888888888888888888888888888888888888888888888888888888",         
+            };
+			Console.WriteLine("\n\n");
+			foreach(string line in arr )
+			Console.WriteLine(line);
+
+			Console.WriteLine("Prepare to lose again loser!");
+			StartGame();
+			}
+
+		public void StartGame()
+		{
+			Console.WriteLine("Enter your name: ");
+			string userName = Console.ReadLine();
+			Console.WriteLine("Enter a number between 1-100. I promise I won't peek!");
+			int userNumber = int.Parse(Console.ReadLine());
+			Game newGame = new Game(userName, userNumber);
+			newGame.MakeComputerGuess();
 		}
-		// public void StartGame()
-		// {
-		// Console.WriteLine("Enter your name: ");
-		// string userName = Console.ReadLine();
-    // Console.WriteLine("Enter a number between 1-100. I promise I won't peek!");
-		// int userNumber = Int.Parse(Console.ReadLine());
-		// Game newGame = new Game(userName, userNumber);
-		// MakeComputerGuess();
-		// }
 }
 
 public class Program
@@ -94,14 +145,9 @@ public class Program
 	// }
 		Console.WriteLine("Enter your name: ");
 		string userName = Console.ReadLine();
-    Console.WriteLine("Enter a number between 1-100. I promise I won't peek! If I can't guess it in five turns, you  win.");
+    Console.WriteLine("Enter a number between 1-100. I promise I won't peek! If I can't guess it in six turns, you win.");
 		int userNumber = int.Parse(Console.ReadLine());
 		Game newGame = new Game(userName, userNumber);
 		newGame.MakeComputerGuess();
 	}
 }
-
-
-
-
-
